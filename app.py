0db6fee456bf08d7c6781d0756e72df8c1dab561
ECH-CHADLI HAMZA <hamzaechadli@gmail.com>
2023-12-11 12:49:46 +0100

    second commit

--- app.py
+++ app.py
@@ -13,22 +13,37 @@ def index():
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    model = pulp.LpProblem('linear_programming', LpMaximize)
+    max_min = request.form.get('max_min')
+
+    model = pulp.LpProblem('linear_programming', LpMaximize if max_min == 'max' else LpMinimize)
     solver = getSolver('PULP_CBC_CMD') 
     x1 = LpVariable('x1', lowBound = 0, cat = 'continuous') 
     x2 = LpVariable('x2', lowBound = 0, cat = 'continuous')
 
-    z1 = int(request.form['z1'])
-    z2 = int(request.form['z2'])
-    a1 = int(request.form['a1'])
-    b1 = int(request.form['b1'])
-    c1 = int(request.form['c1'])
-    a2 = int(request.form['a2'])
-    b2 = int(request.form['b2'])
-    c2 = int(request.form['c2'])
+    z1 = float(request.form['z1'])
+    z2 = float(request.form['z2'])
+
+    a1 = float(request.form['a1'])
+    b1 = float(request.form['b1'])
+    c1 = float(request.form['c1'])
+    a2 = float(request.form['a2'])
+    b2 = float(request.form['b2'])
+    c2 = float(request.form['c2'])
+
+    sc1symb = request.form.get('sc-1-symb')
+    sc2symb = request.form.get('sc-2-symb')
+
     model += z1*x1 + z2*x2
-    model += a1*x1 + b1*x2 <= c1 
-    model += a2*x1 + b2*x2 <= c2
+    if sc1symb == '1':
+        model += a1*x1 + b1*x2 >= c1 
+    else:
+        model += a1*x1 + b1*x2 <= c1 
+
+    if sc2symb == '1':
+        model += a2*x1 + b2*x2 >= c2
+    else:
+        model += a2*x1 + b2*x2 <= c2
+        
     results = model.solve(solver=solver)
     if LpStatus[results] == 'Optimal': print('The solution is optimal.')
     print(f'Objective value: z* = {value(model.objective)}')
@@ -37,12 +52,12 @@ def calculate():
     x_vals = np.linspace(0, 10, 400)
     y1 = (c1 - a1 * x_vals) / b1  
     y2 = (c2 - a2 * x_vals) / b2
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(8, 6)) # create new figure for plotting 8x6 inches
     plt.plot(x_vals, y1, label=f'{a1}*x1 + {b1}*x2 <= {c1}')
     plt.plot(x_vals, y2, label=f'{a2}*x1 + {b2}*x2 <= {c2}')
     yy = np.minimum(y1, y2)
     plt.fill_between(x_vals, 0, yy, alpha=0.2, color='gray', label='Feasible Region')
-    plt.plot(value(x1), value(x2), 'ro')  # Optimal solution point
+    plt.plot(value(x1), value(x2), 'ro')  # Optimal solution point (r: red, o: circle marker)
     plt.xlabel('x1')
     plt.ylabel('x2')
     plt.title('Feasible Region and Optimal Solution')
@@ -59,9 +74,6 @@ def calculate():
     img.seek(0)
     plot_url = base64.b64encode(img.getvalue()).decode()
     img.close()
-
-    
-
     
     return render_template('result.html', plot_url=plot_url)  # Return the result to be displayed
 
